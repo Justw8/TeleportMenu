8ac62236d1bc0288ab5ecb4d6476c93c8e1ec7ce
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2024-08-27 08:37:17 +0800

    Update zhTW.lua

--- Locales/zhTW.lua
+++ Locales/zhTW.lua
@@ -31,3 +31,5 @@ if not L then return end
 --L["Enable Tooltip"] = "Enable/Disable the Teleport Menu."
 --L["Hearthstone Toy"] = "Hearthstone Toy"
 --L["Hearthstone Toy Tooltip"] = "Sets the preferred hearthstone toy to use.\n\nIf set to Random, a random hearthstone toy will be choosen every time you open the game menu."
+L["None"] = "無"
+L["Random"] = "|T1669494:16:16:0:0:64:64:4:60:4:60|t 隨機"
